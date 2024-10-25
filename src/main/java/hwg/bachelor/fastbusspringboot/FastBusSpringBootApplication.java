ec3edd64315ffa81a3957c9175b43ba7ce324d9a
Ali <ali.duendar@outlook.de>
2024-10-25 14:36:45 +0200

    init

---
+++ src/main/java/hwg/bachelor/fastbusspringboot/FastBusSpringBootApplication.java
@@ -0,0 +1,13 @@
+package hwg.bachelor.fastbusspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastBusSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastBusSpringBootApplication.class, args);
+    }
+
+}
